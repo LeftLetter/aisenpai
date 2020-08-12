c9e3236a5e8d446f418b82d99ec4c5e661d605fc
LeftLetter <zmsh_white@yahoo.co.jp>
2020-08-12 10:04:16 +0000

    ESLint設定変更による修正

--- scenario-server/common/logger.js
+++ scenario-server/common/logger.js
@@ -1,12 +1,12 @@
 const log4js = require('log4js')
 
 class Logger {
-  constructor () {
+  constructor() {
     log4js.configure(global.LOG4JS_CONFIG_FILE)
     this.logger = log4js.getLogger('app')
   }
 
-  getLogger () {
+  getLogger() {
     return this.logger
   }
 }
